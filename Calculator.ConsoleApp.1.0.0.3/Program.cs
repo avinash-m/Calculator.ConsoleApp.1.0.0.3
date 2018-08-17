2730069086beeb71f04dcbca68398f81868bb936
Avinash M <avinashm201993@gmail.com>
2018-08-17 12:16:09 -0500

    Added class library and added it as reference.

--- Calculator.ConsoleApp.1.0.0.3/Program.cs
+++ Calculator.ConsoleApp.1.0.0.3/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Avinash M
+    08/17/2018
+    Caluclator application with functionalities imported from custom made class library
+ 
+ */
+
+
+using System;
+using MathLibrary; // Using custom class library
 
 namespace Calculator.ConsoleApp._1._0._0._3
 {
@@ -10,6 +15,40 @@ namespace Calculator.ConsoleApp._1._0._0._3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*** Calculator App ***");
+            Console.Write("Enter first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());            
+            Console.Write("Enter second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            // Avinash Program code------------------------------------
+
+            Console.WriteLine("\n--------------Avinash Math Operations---------------------");
+
+            MathOperations.Addition(firstNumber, secondNumber);
+            MathOperations.Subtraction(firstNumber, secondNumber);
+            MathOperations.Multiplication(firstNumber, secondNumber);
+            MathOperations.Division(firstNumber, secondNumber);
+            
+            Console.WriteLine("-----------------------------------------------------------");
+
+            // Durga Program code------------------------------------
+
+
+            Console.WriteLine("\n--------------Durga Math Operations---------------------\n");
+
+            DurgaMathOperations mathoperations = new DurgaMathOperations();
+
+            mathoperations.addition(firstNumber, secondNumber);
+            mathoperations.subtraction(firstNumber, secondNumber);
+            mathoperations.multiplication(firstNumber, secondNumber);
+            mathoperations.division(firstNumber, secondNumber);
+            mathoperations.Reminder(firstNumber, secondNumber);
+
+            Console.WriteLine("\nPress any key to continue....................");
+            Console.ReadKey();
         }
     }
+
+    
 }
---
+++ MathLibrary/MathOperations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    // Avinash
+    public static class MathOperations
+    {
+        static MathOperations()
+        {
+            Console.WriteLine("--- In Static Class ---\n");
+        }
+
+        public static void Addition(double firstNumber, double secondNumber)
+        {
+            if (firstNumber > 0 && secondNumber > 0)
+            {
+                double sum = firstNumber + secondNumber;
+                Console.WriteLine($"Addition of numbers: { sum }");
+            }
+            else
+            {
+                Console.WriteLine("Additon: Numbers should be greater than zero.");
+            }
+        }
+
+        public static void Subtraction(double firstNumber, double secondNumber)
+        {
+            if (firstNumber != 0 && secondNumber != 0)
+            {
+                double sum = firstNumber - secondNumber;
+                Console.WriteLine($"Subtraction of numbers: { sum }");
+            }
+            else
+            {
+                Console.WriteLine("Subtraction of two zeros is zero");
+            }
+        }
+
+        public static void Multiplication(double firstNumber, double secondNumber)
+        {
+            decimal Multiplier = (decimal)firstNumber * (decimal)secondNumber;
+
+            Console.WriteLine("Multiplication of Numbers: {0}", Multiplier.ToString());
+        }
+
+        public static void Division(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                double quotient = firstNumber / secondNumber;
+                Console.WriteLine($"Division of Numbers: {quotient:0.000}");
+            }
+            else
+            {
+                Console.WriteLine("Division: Denominator cannot be zero.");
+            }
+
+        }
+
+        public static void Remainder(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                double remainder = firstNumber % secondNumber;
+                Console.WriteLine($"Remainder of Numbers: {remainder:0.000}");
+            }
+            else
+            {
+                Console.WriteLine("Remainder: Denominator cannot be zero.");
+            }
+
+        }
+    }
+
+    
+    
+}
